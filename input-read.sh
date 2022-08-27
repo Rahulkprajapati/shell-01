f10bccc2d3b7ab9362e2ede88f2eca4207b117c2
Rahulkrajapati <rahulkpkprajapati147@gmail.com>
2022-08-28 02:08:46 +0530

    multiple i/o:main

--- input-read.sh
+++ input-read.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 echo "Select program to run:   "     #e.g. allow user to select program to run 
-read program_name                            #your input will be saved in this variable "program_name"
+read program_name                    #your input will be saved in this variable "program_name"
+read program_name1 program_name2     #you can input multiple read variable
 echo "Selected program: $program_name"
