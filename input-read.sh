ed5aed968aab0b379ae4785873f8b202fc21231a
Rahulkrajapati <rahulkpkprajapati147@gmail.com>
2022-08-28 02:01:52 +0530

    how to input:main

---
+++ input-read.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo "Select program to run:   "     #e.g. allow user to select program to run 
+read program_name                            #your input will be saved in this variable "program_name"
+echo "Selected program: $program_name"
