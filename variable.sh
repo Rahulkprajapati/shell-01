5efeb9a11327fc59d74346bc0ed939f271c86ea2
Rahulkrajapati <rahulkpkprajapati147@gmail.com>
2022-08-28 01:46:39 +0530

    variables:main

---
+++ variable.sh
@@ -0,0 +1,19 @@
+#Variables
+
+#Store data any kind of
+#two types of variables
+#1 system defined
+#maintained and created by linux and defined in capital cases
+#2 user defined variable
+# maintained and created by user no strict to use cases
+
+# // sytem defiend variable $ //
+echo $BASH   
+echo $BASH_VERSION
+echo $HOME
+echo $PWD
+
+#User defined variable
+
+city=banglore               #variable assigne
+echo Karnataka capital $city
